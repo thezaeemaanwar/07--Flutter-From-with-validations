4dd67db64ed8e2d06830e1517b0ec6c40193b1bf
Zaeema Anwar <thezaeemaanwar@gmail.com>
2021-05-25 09:04:34 +0500

    📦 NEW : Signup form and validations

--- lib/main.dart
+++ lib/main.dart
@@ -5,109 +5,213 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Form Validation"),
+      ),
+      body: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  final title;
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // final formKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> formKey = new GlobalKey<FormState>();
+  String name, email, phone, password;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  bool checkboxVal = false;
+  bool isPasswordCompliant(String password) {
+    bool isComplient = false;
+    bool hasUppercase = false;
+    bool hasDigits = false;
+    bool hasLowercase = false;
+    bool hasSpecialCharacters = false;
+    var character = '';
+    var i = 0;
+    if (password.isNotEmpty) {
+      hasSpecialCharacters =
+          password.contains(new RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+      while (i < password.length) {
+        character = password.substring(i, i + 1);
+        if (isDigit(character, 0)) {
+          hasDigits = true;
+        } else {
+          if (character == character.toUpperCase()) {
+            hasUppercase = true;
+          }
+          if (character == character.toLowerCase()) {
+            hasLowercase = true;
+          }
+        }
+        i++;
+      }
+    }
+    isComplient =
+        hasDigits & hasUppercase & hasLowercase & hasSpecialCharacters;
+    return isComplient;
   }
 
+  bool isDigit(String s, int idx) =>
+      "0".compareTo(s[idx]) <= 0 && "9".compareTo(s[idx]) >= 0;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        body: SingleChildScrollView(
+            child: Column(children: [
+      Column(children: [
+        SizedBox(height: 40),
+        Text(
+          "Register",
+          style: TextStyle(fontSize: 30),
+        ),
+        Text("Signup to experience new ways")
+      ]),
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 70),
+        // width: 50.0,
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 40.0)),
+              TextFormField(
+                onSaved: (value) => {
+                  this.setState(() {
+                    name = value;
+                  })
+                },
+                decoration: InputDecoration(
+                    labelText: 'Name *',
+                    fillColor: Colors.blue,
+                    icon: Icon(
+                      Icons.person_outline,
+                      color: Colors.deepOrange,
+                    )),
+                validator: (String value) {
+                  return (value == null)
+                      ? 'Do not leave the name empty.'
+                      : null;
+                },
+                keyboardType: TextInputType.name,
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                onSaved: (value) => {},
+                decoration: InputDecoration(
+                    labelText: 'Email ID *',
+                    fillColor: Colors.blue,
+                    icon: Icon(
+                      Icons.email_outlined,
+                      color: Colors.deepOrange,
+                    )),
+                validator: (String value) {
+                  return (value == null)
+                      ? 'Email ID is required'
+                      : (!value.contains('@') || !value.contains('.'))
+                          ? 'Invalid Email'
+                          : null;
+                },
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                onSaved: (value) => {},
+                decoration: InputDecoration(
+                    labelText: 'Contact Number',
+                    fillColor: Colors.blue,
+                    icon: Icon(
+                      Icons.phone_outlined,
+                      color: Colors.deepOrange,
+                    )),
+                validator: (String value) {
+                  return (value == null)
+                      ? 'Contact Number is required'
+                      : (value.length == 11 && int.parse(value) != null)
+                          ? null
+                          : 'Invalid Contact Number';
+                },
+                keyboardType: TextInputType.phone,
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                onSaved: (value) => {},
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    fillColor: Colors.blue,
+                    icon: Icon(
+                      Icons.lock_outline,
+                      color: Colors.deepOrange,
+                    )),
+                validator: (String value) {
+                  return (value == null)
+                      ? 'You must create a password first'
+                      : (value.length > 5)
+                          ? (isPasswordCompliant(value))
+                              ? null
+                              : 'Password must contain uppercase and lowercase letters, special letters and digits'
+                          : 'Psssword length must be greater than 5';
+                },
+                keyboardType: TextInputType.visiblePassword,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => {
+                  // skey.currentState.sa
+                  if (checkboxVal)
+                    {
+                      if (formKey.currentState.validate())
+                        {this.formKey.currentState.save()}
+                    }
+                  else
+                    {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text(
+                              'Please accept the terms and conditions forst')))
+                    }
+                },
+                child: Text("Register"),
+                style: ButtonStyle(
+                  alignment: Alignment.center,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      Row(
+        children: [
+          Checkbox(
+              value: checkboxVal,
+              onChanged: (bool val) => {
+                    this.setState(() {
+                      checkboxVal = val;
+                    })
+                  }),
+          Text("I have read and accept Terms and Conditions")
+        ],
+      )
+    ])));
   }
 }
